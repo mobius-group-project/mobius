5f32f9c1003142e858e7646728aac43603d0999f
Katsiaryna Novikava <novikova556699@gmail.com>
2026-05-09 22:01:24 +0200

    mg-30: basic setup for calendar module

---
+++ src/components/calendarSystem/sampleEvents.ts
@@ -0,0 +1,30 @@
+// src/CalendarSystem/sampleEvents.ts
+// src/CalendarSystem/sampleEvents.ts
+import type { CalendarEvent } from "./eventTypes";
+
+export const sampleEvents: CalendarEvent[] = [
+  {
+    id: 1,
+    title: "Plan 1",
+    day: 0,
+    startTime: "08:00",
+    endTime: "09:00",
+    color: "#FFB347"
+  },
+  {
+    id: 2,
+    title: "Plan 2",
+    day: 0,
+    startTime: "10:00",
+    endTime: "11:00",
+    color: "#77DD77"
+  },
+  {
+    id: 3,
+    title: "Plan 3",
+    day: 2,
+    startTime: "12:00",
+    endTime: "13:00",
+    color: "#AEC6CF"
+  }
+];
