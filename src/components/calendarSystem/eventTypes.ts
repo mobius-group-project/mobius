13f76b23688994a7b9c75021f1d18ff00184ad2d
Katsiaryna Novikava <novikova556699@gmail.com>
2026-05-14 20:58:17 +0200

    Squashed commit of the following: fix bug with task addition
    commit 8dc76edc096ee2cfa0f4892743081c3fe0f209f9
    Author: Katsiaryna Novikava <novikova556699@gmail.com>
    Date:   Thu May 14 20:57:36 2026 +0200
    
        fix for MG-32

--- src/components/calendarSystem/eventTypes.ts
+++ src/components/calendarSystem/eventTypes.ts
@@ -1,11 +1,8 @@
-
-export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
-
 export interface CalendarEvent {
   id: number;
   title: string;
-  day: DayOfWeek;
+  date: string;      // "YYYY-MM-DD"
   startTime: string; // "HH:MM"
   endTime: string;   // "HH:MM"
-  color: string;     // hex color
+  color: string;
 }
